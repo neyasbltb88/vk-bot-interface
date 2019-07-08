217280a4de1b18c613da8aca40710336b9265837
Neyasbltb <neyasbltb_88@mail.ru>
2019-07-08 14:09:55 +0300

    Инициализация скрипта по событию загрузки страницы

--- app/main.js
+++ app/main.js
@@ -19,25 +19,28 @@ Vue.mixin({
 })
 
 // Декоратор роутера VK
-// TODO: скорее всего он тут не будет нужен
 window.navWatcher = new NavWatcher({
     callback: objLoc => console.log('Параметры коллбека перехода: ', objLoc),
 });
 
-// Создаем блок, в который будет монтироваться приложение
-let appElem = document.createElement('div')
-    // id корневого элемента приложения будет соответствовать имени приложения
-appElem.id = APP_NAME
-    // и добавляем его в конец body
-document.body.appendChild(appElem)
+function init() {
+    // Создаем блок, в который будет монтироваться приложение
+    let appElem = document.createElement('div')
+        // id корневого элемента приложения будет соответствовать имени приложения
+    appElem.id = APP_NAME
+        // и добавляем его в конец body
+    document.body.appendChild(appElem)
 
-// Сохраняем экземпляр приложения в глобальную переменную под именем приложения
-window[APP_NAME] = new Vue({
-    router,
-    store,
-    created() {
-        // Заносим имя приложения в store
-        this.$store.commit('setAppName', APP_NAME)
-    },
-    render: h => h(App)
-}).$mount(`#${APP_NAME}`)
+    // Сохраняем экземпляр приложения в глобальную переменную под именем приложения
+    window[APP_NAME] = new Vue({
+        router,
+        store,
+        created() {
+            // Заносим имя приложения в store
+            this.$store.commit('setAppName', APP_NAME)
+        },
+        render: h => h(App)
+    }).$mount(`#${APP_NAME}`)
+}
+
+window.addEventListener('load', init)
