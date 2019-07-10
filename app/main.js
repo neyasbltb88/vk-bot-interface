811b49709a6aaa769551a57c74b1c3ab59096483
Neyasbltb <neyasbltb_88@mail.ru>
2019-07-10 16:43:46 +0300

    Налажен запуск интерфейса в нужной беседе

--- app/main.js
+++ app/main.js
@@ -3,75 +3,54 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import NavWatcher from './scripts/nav-watcher'
-import launcher from './scripts/launcher'
+import Launcher from './scripts/launcher'
 
 Vue.config.productionTip = false
 
 // Имя приложения
 const APP_NAME = 'bot_interface'
-const CHAT_NAME = 'БОТ'
-const select = x => document.querySelector(x);
-const selectAll = x => document.querySelectorAll(x);
-const newEl = x => document.createElement(x);
-
-// С помощью глобального миксина добавляем APP_NAME в data всех компонентов
-Vue.mixin({
-    data() {
-        return {
-            APP_NAME,
-            CHAT_NAME
-        }
-    }
-})
-
-function checkChatName() {
-    let chatHeader = select('.im-page--title-main-inner');
-    return chatHeader && chatHeader.textContent === CHAT_NAME;
+    // Селектор контейнера приложения
+const APP_CONTAINER_SELECTOR = `#${APP_NAME}`
+    // Параметры нашей беседы
+const CHAT_PARAMS = {
+    NAME: 'БОТ',
+    ID: undefined
 }
 
-// Декоратор роутера VK
-window.navWatcher = new NavWatcher({
-    callback: objLoc => {
-        console.log('Параметры коллбека перехода: ', objLoc)
+let wasInit = false
 
-        let regex = /c(\d+)/mi;
-        let chat_id = null;
-        let res = null;
+const select = x => document.querySelector(x)
+const selectAll = x => document.querySelectorAll(x)
+const newEl = x => document.createElement(x)
 
-        // Если мы зашли в любую беседу
-        if (objLoc.sel && (res = objLoc.sel.match(regex))) {
-            chat_id = res[1];
-            console.log('Оп! Мы вбеседе с id: ', chat_id);
+// Запускает инициализацию основного кода приложения
+const initBotInterface = () => {
+    if (wasInit) return
+    wasInit = true
 
-            // Работает определение беседы, но без ограничения в attempts
-            // экзкмпляры лаунчера стакаются при каждом заходе в любую беседу.
-            // Нужно сделать launcher классом с методами launch и stop
-            // и не допускать внутри класса их повторные вызызовы.
-            launcher({
-                condition: checkChatName,
-                callback() {
-                    console.log('Мы в нужной беседе');
-                },
-                attempts: 100
-            })
-
-            // Если мы вообще не в беседе
-        } else {
-
-        }
+    // Если на странице еще нет контейнера приложения
+    if (!select(APP_CONTAINER_SELECTOR)) {
+        // Создаем блок, в который будет монтироваться приложение
+        let appElem = newEl('div')
+            // id корневого элемента приложения будет соответствовать имени приложения
+        appElem.id = APP_NAME
+            // и добавляем его в конец body
+        document.body.appendChild(appElem)
     }
-});
 
-function init() {
-    // Создаем блок, в который будет монтироваться приложение
-    let appElem = newEl('div')
-        // id корневого элемента приложения будет соответствовать имени приложения
-    appElem.id = APP_NAME
-        // и добавляем его в конец body
-    document.body.appendChild(appElem)
+    // С помощью глобального миксина добавляем APP_NAME в data всех компонентов
+    Vue.mixin({
+        data() {
+            return {
+                APP_NAME,
+                APP_CONTAINER_SELECTOR,
+                CHAT_PARAMS
+            }
+        }
+    })
 
     // Сохраняем экземпляр приложения в глобальную переменную под именем приложения
-    window[APP_NAME] = new Vue({
+    window[`$${APP_NAME}`] = new Vue({
         router,
         store,
         created() {
@@ -79,11 +58,95 @@ function init() {
             this.$store.commit('setAppName', APP_NAME)
         },
         render: h => h(App)
-    }).$mount(`#${APP_NAME}`)
+    }).$mount(APP_CONTAINER_SELECTOR)
 }
 
-// TODO: Тут надо так же запускать старт лаунчера
-// Если скрипт запустился уже в нужной беседе, то сразу активировать основную логику
-if (checkChatName()) init();
+// Уничтожает инстанс интерфейса и удаляет его со страницы
+const destroyBotInterface = () => {
+    if (!wasInit) return
+    wasInit = false
 
-// window.addEventListener('load', init)
+    if (window[`$${APP_NAME}`]) {
+        window[`$${APP_NAME}`].$destroy()
+        delete window[`$${APP_NAME}`]
+    }
+
+    let appElem = select(APP_CONTAINER_SELECTOR)
+    if (appElem) appElem.innerHTML = ''
+}
+
+// Возвращает true если заголовок диалога совпадает с нашей беседой
+const checkChatName = () => {
+    let result = false;
+    let chatHeader = select('.im-page--title-main-inner')
+
+    if (chatHeader && chatHeader.textContent === CHAT_PARAMS.NAME) {
+        result = true
+    }
+
+    return result;
+}
+
+// Проверяет находимся ли мы в нужной беседе. 
+// Принимает объект локации родного роутера вк(window.nav.objLoc)
+const checkNavToChat = (objLoc) => {
+    // console.log('Параметры коллбека перехода: ', objLoc)
+    let regex = /c(\d+)/mi
+    let chat_id = null
+    let res = null
+
+    // Если мы зашли в любую беседу
+    if (objLoc[0] === 'im' && objLoc.sel && (res = objLoc.sel.match(regex))) {
+        chat_id = res[1];
+
+        // Если уже ранее был найден id нашей беседы, и сейчас мы в ней
+        if (CHAT_PARAMS.ID && CHAT_PARAMS.ID === chat_id) {
+            initBotInterface()
+
+            // Если уже ранее был найден id нашей беседы, и сейчас мы НЕ в ней
+        } else if (CHAT_PARAMS.ID && CHAT_PARAMS.ID !== chat_id) {
+            destroyBotInterface()
+
+            // Если id нашей беседы еще не найден, продолжаем искать
+        } else if (!CHAT_PARAMS.ID) {
+            LAUNCHERS.init.run(chat_id)
+        }
+
+        // Если мы вообще не в беседе
+    } else {
+        LAUNCHERS.init.stop()
+        destroyBotInterface()
+    }
+}
+
+// Декоратор роутера VK
+window.navWatcher = new NavWatcher({
+    callback: checkNavToChat
+});
+
+const LAUNCHERS = {
+        // Лаунчер, срабатывающий каждый раз при переходе по страницам ВК.
+        // Вызывает инициализацию интерфейса, если мы заходим в нашу беседу 
+        // с любой другой страницы ВК
+        init: new Launcher({
+            condition: checkChatName,
+            callback(chat_id) {
+                CHAT_PARAMS.ID = chat_id
+
+                initBotInterface()
+            },
+            attempts: 100
+        }),
+        // Лаунчер, срабатывающий один раз при запуске этого скрипта.
+        // Вызывает инициализацию интерфейса, если этот скрипт загрузился сразу в нашей беседе
+        navReady: new Launcher({
+            condition: () => window.nav && window.nav.objLoc,
+            callback: () => checkNavToChat(window.nav.objLoc)
+        })
+    }
+    // Для доступа к лаунчерам из консоли во время отладки
+window[`${APP_NAME}_LAUNCHERS`] = LAUNCHERS;
+
+// Точка входа. Запускает ожидание родного роутера ВК для анализа страницы, 
+// на которой запустился скрипт
+LAUNCHERS.navReady.run()
--- app/scripts/launcher.js
+++ app/scripts/launcher.js
@@ -1,15 +1,55 @@
-export default function launcher(params = {}) {
-    let { condition, callback, arg, attempts = Infinity } = params;
-    let try_num = 0;
+export default class Launcher {
+    running = false;
+    wasStopped = false;
+    tryNum = 0;
 
-    requestAnimationFrame(function launch(arg) {
-        if (!condition.call() && try_num < attempts) {
-            try_num++;
-            requestAnimationFrame(launch.bind(this, arg));
-        } else if (condition.call()) {
-            callback.call(this, arg);
-        } else {
+    constructor(props = {}) {
+        this.condition = props.condition || true;
+        this.callback = props.callback || console.log.bind(this, 'В Laucher не передан callback');
+        this.arg = props.arg;
+        this.attempts = props.attempts || Infinity;
+
+        if (props.autoRun) this.run();
+    }
+
+    launch = () => {
+        if (!this.condition.call(this) && this.tryNum < this.attempts && !this.wasStopped) {
+            this.tryNum++;
+            console.log('this.tryNum: ', this.tryNum);
+
+            requestAnimationFrame(this.launch);
+        } else if (this.condition.call(this) && this.tryNum < this.attempts && !this.wasStopped) {
+            this.stop();
+            this.callback(this.arg);
+        } else if (this.tryNum >= this.attempts && !this.wasStopped) {
             console.log('Превышено количество попыток launcher');
+
+            this.stop();
+        } else if (this.wasStopped) {
+            console.log('launcher остановлен');
         }
-    }.bind(this, arg))
+    }
+
+    run = (arg) => {
+        if (this.running) return;
+        if (arg !== undefined) this.arg = arg;
+        console.log('Launcher.run()');
+
+        this.running = true;
+        this.wasStopped = false;
+        this.launch();
+
+        return true;
+    }
+
+    stop = () => {
+        if (!this.running) return;
+        console.log('Launcher.stop()');
+
+        this.wasStopped = true;
+        this.running = false;
+        this.tryNum = 0;
+
+        return true;
+    }
 }
